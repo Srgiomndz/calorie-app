c3c2f83a675460ecebcab3db64e4445dcc5787c9
Sergio Mendez <sergio.mendez.roca@gmail.com>
2022-12-16 12:24:11 -0800

    added reset button for daily entries view

--- routes/main.js
+++ routes/main.js
@@ -11,6 +11,7 @@ router.get('/', homeController.getIndex);
 router.get('/profile', ensureAuth, postsController.getProfile);
 router.get('/dashboard', ensureAuth, postsController.getDashboard);
 router.post('/dashboard/entry', profileController.addNewEntry);
+router.post('/dashboard/reset', profileController.resetEntries);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/logout', authController.logout);
