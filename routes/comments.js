87d1633256b5f889ce323874231bcaf41fc11dc6
Sergio Mendez <sergio.mendez.roca@gmail.com>
2022-12-18 18:12:19 -0800

    work on post and comments pages

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
 
+
+
 router.post("/createComment/:id", commentsController.createComment);
 
 router.put("/likeComment/:id", commentsController.likeComment);
